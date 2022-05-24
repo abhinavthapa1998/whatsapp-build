f2eeaf4f27592fc07eb8933894086a8459774b2e
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-05-24 20:01:32 +0530

    First

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,28 @@
+import { Avatar, IconButton } from "@mui/material";
+import styled from "styled-components";
+import { Chat } from "@mui/material";
+import { MoreVert } from "@mui/material";
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <Chat />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+    </Container>
+  );
 }
 
 export default Sidebar;
+
+const Container = styled.div``;
+const Header = styled.div``;
+const UserAvatar = styled(Avatar)``;
+const IconsContainer = styled.div``;
