a892be8821f3bac735c15b3f77196b390e7f1a92
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-05-27 22:58:46 +0530

    Midway

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,18 @@
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, IconButton, Button } from "@mui/material";
 import styled from "styled-components";
 import { Chat } from "@mui/material";
 import { MoreVert } from "@mui/material";
+import { SearchOutlined } from "@material-ui/icons";
+import * as EmailValidator from "email-validator";
 function Sidebar() {
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email address for the user you wish to chat with"
+    );
+    if (!input) return null;
+    if (EmailValidator.validate(input)) {
+    }
+  };
   return (
     <Container>
       <Header>
@@ -16,6 +26,11 @@ function Sidebar() {
           </IconButton>
         </IconsContainer>
       </Header>
+      <Search>
+        <SearchOutlined />
+        <SearchInput />
+      </Search>
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
     </Container>
   );
 }
@@ -23,6 +38,40 @@ function Sidebar() {
 export default Sidebar;
 
 const Container = styled.div``;
-const Header = styled.div``;
-const UserAvatar = styled(Avatar)``;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+const SidebarButton = styled(Button)`
+  width: 100%;
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
 const IconsContainer = styled.div``;
---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "./firebase";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkHJRwStarCMIG395-L52K3mSaYcFmXTg",
+  authDomain: "whatsapp-build-627c3.firebaseapp.com",
+  projectId: "whatsapp-build-627c3",
+  storageBucket: "whatsapp-build-627c3.appspot.com",
+  messagingSenderId: "1095134652191",
+  appId: "1:1095134652191:web:b94c1d58a2edab41d2322a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider };
